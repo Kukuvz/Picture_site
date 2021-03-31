c88c8b1ca387418c53bcb2a23fc55f3ec024fd84
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-03-31 09:20:59 +0300

    add burger

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -3,18 +3,24 @@ const accordion = (triggersSelector) => {
 
     btns.forEach(btn => {
         btn.addEventListener('click', function () {
-            this.classList.toggle('active-style');
-            this.nextElementSibling.classList.toggle('active-content');
-
+            
             if (this.classList.contains('active-style')) {
-
-                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
-            } else {
-
+                this.classList.remove('active-style');
+                this.nextElementSibling.classList.remove('active-content');
                 this.nextElementSibling.style.maxHeight = '0px';
+            } else {
+                btns.forEach(btn => {
+                    btn.classList.remove('active-style');
+                    btn.nextElementSibling.classList.remove('active-content');
+                    btn.nextElementSibling.style.maxHeight = '0px';
+                });
+                this.classList.add('active-style');
+                this.nextElementSibling.classList.add('active-content');
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
             }
         });
     });
+
 
     //   blocks = document.querySelectorAll(itemsSelector);
 
